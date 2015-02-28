7a23b6f23499ae6673ab7f488ffedb185a709ded
Davidslc <djjohnson@gmail.com>
2015-02-28 14:48:35 -0700

    removed trailing slash in front of recipes in urls.py

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -4,7 +4,7 @@ from views import *
 urlpatterns = patterns(
     '',
 
-    url(r'^recipes/$', RecipeList.as_view(), name='recipe-list'),
+    url(r'^recipes$', RecipeList.as_view(), name='recipe-list'),
     url(r'^recipes/(?P<pk>[0-9]+)$', RecipeDetail.as_view(), name='recipe-list'),
     url(r'^add-recipe$', AddRecipe.as_view(), name='add-recipe'),
     url(r'^ingredients$', IngredientList.as_view(), name='ingredient-list'),
